cc39324a122c280926a88bc9b92d162d08c34624
Africa Unite <directcastexception@gmail.com>
2022-08-08 17:50:34 +0800

    fixed multiplier decimal

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09fbec6ab9a2f3279b4541da491f5763.js"
+  "/precache-manifest.930b104be7aea88ed19d40ba7896ccd8.js"
 );
 
 workbox.clientsClaim();
